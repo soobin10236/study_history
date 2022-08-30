0b4502f5fc300c7f50e5cd2f9f20859a9435373f
soobin lee <tnqls7742@gmail.com>
2022-08-30 22:56:41 +0900

    second commit

---
+++ src/main/java/hello/core/MemberApp.java
@@ -0,0 +1,28 @@
+package hello.core;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+
+public class MemberApp {
+    public static void main(String[] args) {
+
+        // AppConfig appConfig = new AppConfig();
+        // MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class); //스프링 컨테이너 생성
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class); //(이름, 타입)
+
+
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("member : " + member.getName());
+        System.out.println("findeMember : " + findMember.getName());
+    }
+}
---
+++ src/main/java/hello/core/OrderApp.java
@@ -0,0 +1,33 @@
+package hello.core;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.order.Order;
+import hello.core.order.OrderService;
+
+public class OrderApp {
+    
+    public static void main(String[] args) {
+
+        // AppConfig appConfig = new AppConfig();
+        // MemberService memberService = appConfig.memberService();
+        // OrderService orderService = appConfig.orderService();
+
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class); //스프링컨테이너 생성
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        OrderService orderService = ac.getBean("orderService", OrderService.class);
+
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 20000);
+
+        System.out.println("order : " + order);
+
+    }
+}
